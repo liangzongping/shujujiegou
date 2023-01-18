306309bffafb6f5a1d8c24b83bad784636b7d07c
梁宗平 <536190198@qq.com>
2023-01-18 15:38:46 +0800

    双链表

--- DLIST.c
+++ DLIST.c
@@ -10,13 +10,16 @@ dlist* dlistinit()
 void dlistpushback( dlist* head,int x)
 {
 	assert(head);
-	dlist* tail = head->prve;
+	/*dlist* tail = head->prve;
 	dlist* newhead= (dlist*)malloc(sizeof(dlist));
 	newhead->val = x;
 	tail->next = newhead;
 	newhead->prve = tail;
 	newhead->next = head;
-	head->prve = newhead;
+	head->prve = newhead;*/
+	//利用插入来实现尾插
+	//dlistInsertfront(head, x);
+	dlistInsertback(head->prve, x);
 }
 void dlistprint(dlist* head)
 {
@@ -38,25 +41,103 @@ void dlistpushfront(dlist* head, int x)
 	head->next = newhead;
 	newhead->prve = tail->prve;//错误做法
 	tail->prve = newhead;*/
-	dlist *newhead=(dlist*)malloc(sizeof(dlist));//正确做法
-	newhead->val = x;
-	dlist* next = head->next;
+	//正确做法
+	
+	dlist *newhead=(dlist*)malloc(sizeof(dlist));
+	/*dlist* next = head->next;
 	head->next = newhead;
 	newhead->prve = head;
 	newhead->next = next;
-	next->prve = newhead;
-
-
+	next->prve = newhead;*/
+	
+	//利用插入来实现头插
+	//dlistInsertfront(head->next , x);
+	dlistInsertback(head, x);
 }
 void dlistdeleback(dlist* head)
 {
 	assert(head);
 	assert(head->next != head);
-	dlist* tail = head->prve;
+	/*dlist* tail = head->prve;
 	dlist* tailprve = tail->prve;
 	free(tail);
 	tailprve->next = head;
-	head->prve = tailprve;
-	
-	
+	head->prve = tailprve;*/
+	//利用插入删除来实现；
+	dlistpop(head->prve);
+}
+void dlistdelefront(dlist* head)
+{
+	assert(head);
+	/*dlist* p = head->next;
+	head->next = p->next;
+	p->next->prve = head;
+	free(p);*/
+	//利用插入删除来实现
+	dlistpop(head->next);
+}
+void dlistInsertfront(dlist* pos,int x)
+{
+	dlist* newhead = (dlist*)malloc(sizeof(dlist));//正确做法
+	newhead->val = x;
+	//int n;
+	//printf("请输入你在第几个后插入\n");
+	//scanf("%d", &n);
+	//dlist* tail = dlistFind(head, n);
+	//dlist* next = tail->next;
+	//newhead->next = next;
+	//next->prve = newhead;
+	//tail->next = newhead;
+	//newhead->prve = tail;
+	dlist* posprve = pos->prve;
+	newhead->next = pos;
+	pos->prve = newhead;
+	newhead->prve = posprve;
+	posprve->next = newhead;
+
+}
+dlist * dlistFind(dlist* head, int x)
+{
+	dlist* tail = head;
+	int i = 0;
+	while (i<x)
+	{
+		tail = tail->next;
+		i++;
+		if (tail == head)
+			return tail;
+	}
+	return tail;
+}
+void dlistInsertback(dlist* pos, int x)
+{
+	assert(pos);
+	dlist* newhead = (dlist*)malloc(sizeof(dlist));//正确做法
+	newhead->val = x;
+	dlist* posnext = pos->next;
+	pos->next = newhead;
+	newhead->prve = pos;
+	newhead->next =posnext;
+	posnext->prve = newhead;
+}
+void dlistpop(dlist* pos)
+{
+	dlist* posprve = pos->prve;
+	dlist* posnext = pos->next;
+	posprve->next = posnext;
+	posnext->prve = posprve;
+	free(pos);
+}
+void dlistdestory(dlist** head)
+{
+	assert(head);
+	dlist* cur = (*head)->next;
+	while (cur->next != *head)
+	{
+		dlist* next = cur->next;
+		free(cur);
+		cur = next;
+	}
+	free(*head);
+	*head = NULL;
 }
--- DLIST.h
+++ DLIST.h
@@ -11,3 +11,9 @@ void dlistpushback(dlist* head,int x);
 void dlistprint(dlist* head);
 void dlistpushfront(dlist* head, int x);
 void dlistdeleback(dlist* head);
+void dlistdelefront(dlist* head);
+dlist * dlistFind(dlist* head, int x);//也可以用二级指针来返回你要找的位置
+void dlistInsertfront(dlist* head,int x);//在第几个数前插入；
+void dlistInsertback(dlist* pos, int x);//在第几个数后插入;
+void dlistpop(dlist* pos);
+void dlistdestory(dlist* head);
